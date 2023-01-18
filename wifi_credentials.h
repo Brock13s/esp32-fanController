2ce3a9c567c00d9ab082b9cdc530e955701943cf
Brock13s <48967600+Brock13s@users.noreply.github.com>
2023-01-18 16:36:04 +1000

    Update wifi_credentials.h

--- wifi_credentials.h
+++ wifi_credentials.h
@@ -1,2 +1,4 @@
 #define WIFI_SSID "BROCKSNETGEAR_AP"
 #define WIFI_PASS "icykayak861"
+//This is fine since I am using a seperaste router and plus someone would have to be really close to me anyway. I dont use that password for anything else just this 
+//router only
